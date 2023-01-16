fbc8f02666f5b2d3995d11a32352c00ffa11e838
Sald1S <saldis.3110@gmail.com>
2023-01-16 13:07:38 +0600

    My Second commit

---
+++ src/main/java/com/saldis/SpringCRM/Bean/WebConfig.java
@@ -0,0 +1,45 @@
+package com.saldis.SpringCRM.Bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasename("classpath:translate");
+        source.setDefaultEncoding("UTF-8");
+        return source;
+    }
+
+    @Bean
+    public CookieLocaleResolver localeResolver(){
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en"));
+        resolver.setCookieName("language");
+        resolver.setCookieMaxAge(3600*24*30);
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor changeInterceptor(){
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry){
+        interceptorRegistry.addInterceptor(changeInterceptor());
+    }
+}
--- src/main/java/com/saldis/SpringCRM/entity/Courses.java
+++ src/main/java/com/saldis/SpringCRM/entity/Courses.java
@@ -9,7 +9,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Courses {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
